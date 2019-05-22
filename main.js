139d5eb527966e539446d1a916677e9b7637d5ef
Vitalii <ilovepinkpony1@gmail.com>
2019-05-22 17:59:35 +0300

    sol

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
 'use strict';
 
 const carousel = document.querySelector('.carousel');
-const nextButton = document.querySelector('.carousel-next');
-const prevButton = document.querySelector('.carousel-previous');
+const nextButton = document.querySelector('.button-next');
+const prevButton = document.querySelector('.button-previous');
 
 nextButton.addEventListener('click', () => {
   carousel.scrollLeft += 390;
+
+  if (carousel.scrollLeft > 0) {
+    prevButton.classList.remove('button-disabled');
+    prevButton.classList.add('button-active');
+  }
+
+  if (carousel.scrollLeft === 910) {
+    nextButton.classList.remove('button-active');
+    nextButton.classList.add('button-disabled');
+  }
 });
 
 prevButton.addEventListener('click', () => {
   carousel.scrollLeft -= 390;
-})
+
+  if (carousel.scrollLeft < 910) {
+    nextButton.classList.remove('button-disabled');
+    nextButton.classList.add('button-active');
+  }
+
+  if (carousel.scrollLeft === 0) {
+    prevButton.classList.remove('button-active');
+    prevButton.classList.add('button-disabled');
+  }
+});
